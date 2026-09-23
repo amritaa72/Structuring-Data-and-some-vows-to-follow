6aa9c8e7d30fb43f1500400ee4a4c23abeba743f
Amritaa <amritaavarshini777@gmail.com>
2026-09-23 12:05:38 +0530

    Implement menu-driven linked list operations

---
+++ menu_driven_linear_linked_list_with_functions.c
@@ -0,0 +1,522 @@
+PS C:\Users\aweso> .\output
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)1
+Enter integer value to insert: 5
+
+ [Success] 5 inserted at the begining.
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)2
+Enter integer value to insert :1
+
+ [SUCCESS] 1 inserted at the end. 
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)2
+Enter integer value to insert :3
+
+ [SUCCESS] 3 inserted at the end. 
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)2
+Enter integer value to insert :5
+
+ [SUCCESS] 5 inserted at the end. 
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)2
+Enter integer value to insert :7
+
+ [SUCCESS] 7 inserted at the end. 
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)2
+Enter integer value to insert :51
+
+ [SUCCESS] 51 inserted at the end. 
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)2
+Enter integer value to insert :66
+
+ [SUCCESS] 66 inserted at the end. 
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)2
+Enter integer value to insert :23
+
+ [SUCCESS] 23 inserted at the end. 
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)2
+Enter integer value to insert :10
+
+ [SUCCESS] 10 inserted at the end. 
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)3
+
+ Current linked list:5->1->3->5->7->51->66->23->10->NULL
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)4
+
+Enter value to delete :3
+
+ [SUCCESS] Node withh value 3 deleted
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)3
+
+ Current linked list:5->1->5->7->51->66->23->10->NULL
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)5
+
+Enter value to search: 66
+
+ [FOUND] Value 66 is present at the position 6 
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)5
+
+Enter value to search: 4
+
+ [NOT FOUND] Value 4 doesnt exist in the list
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)6
+Enter the value to replace : 7
+Enter the new value  : 4
+
+ [SUCCESS] Value updated from 7 to 4 
+
+ FREEING MEMORY BLOCKS AND CLOSING APPLICATION
+PS C:\Users\aweso> 3
+3
+PS C:\Users\aweso> .\output
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)1
+Enter integer value to insert: 4
+
+ [Success] 4 inserted at the begining.
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)1
+Enter integer value to insert: 15
+
+ [Success] 15 inserted at the begining.
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)2
+Enter integer value to insert :23
+
+ [SUCCESS] 23 inserted at the end. 
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)2
+Enter integer value to insert :66
+
+ [SUCCESS] 66 inserted at the end. 
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)2
+Enter integer value to insert :51
+
+ [SUCCESS] 51 inserted at the end. 
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)2
+Enter integer value to insert :23
+
+ [SUCCESS] 23 inserted at the end. 
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)2
+Enter integer value to insert :10
+
+ [SUCCESS] 10 inserted at the end. 
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)3
+
+ Current linked list:15->4->23->66->51->23->10->NULL
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)2
+Enter integer value to insert :59
+
+ [SUCCESS] 59 inserted at the end. 
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)3
+
+ Current linked list:15->4->23->66->51->23->10->59->NULL
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)4
+
+Enter value to delete :59
+
+ [SUCCESS] Node withh value 59 deleted
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)3
+
+ Current linked list:15->4->23->66->51->23->10->NULL
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)5
+
+Enter value to search: 15
+
+ [FOUND] Value 15 is present at the position 1 
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)2
+Enter integer value to insert :45
+
+ [SUCCESS] 45 inserted at the end. 
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)2
+Enter integer value to insert :67
+
+ [SUCCESS] 67 inserted at the end. 
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)3
+
+ Current linked list:15->4->23->66->51->23->10->45->67->NULL
+
+---------------------------------
+   LINKED LIST OPERATIONS     
+ 
+---------------------------------
+ 1. Insert at beginning (head)
+2. Insert at the END (Tail)
+3.Display Entire List
+4. Delete a node in list
+5. Search for a value in the list
+6.Update a value in the list
+.7 Exit the program
+Enter you choice(1-)6
+Enter the value to replace : 3
+Enter the new value  : 34
+
+[ERROR] Value 3 not found, Update failed.
+
+ FREEING MEMORY BLOCKS AND CLOSING APPLICATION
+PS C:\Users\aweso> 
